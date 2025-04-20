e095bf6e99bf0198d616f30db6e7f856aee080eb
VuzLe <tle63917@gmail.com>
2025-04-20 21:27:10 +0700

    to do list

---
+++ 20_4_2025/script_1.js
@@ -0,0 +1,96 @@
+// Ngày tháng
+const date = new Date();
+const options = {
+  weekday: "long",
+  month: "long",
+  day: "numeric",
+  year: "numeric",
+};
+document.getElementById("current-date").textContent = date.toLocaleDateString(
+  "en-US",
+  options
+);
+
+// Thêm task
+const input = document.querySelector("input");
+const form = document.querySelector("form");
+const list = document.querySelector("#to-do");
+const list_done = document.querySelector("#done-task");
+
+let id = 0;
+let todo = 0;
+let done = 0;
+
+const addTask = (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    const text = input.value.trim();
+
+    if (text !== "") {
+      todo++;
+
+      const newTask = document.createElement("div");
+      newTask.classList.add("task");
+
+      const line = document.createElement("div");
+      line.classList.add("content");
+      line.textContent = text;
+
+      newTask.addEventListener("click", () => {
+        addTaskDone(text);
+        todo--;
+        newTask.remove();
+
+        if (todo > 0) {
+          document.getElementById("task-notification").innerHTML = "To Do";
+        } else {
+          document.getElementById("task-notification").innerHTML =
+            "No tasks defined";
+        }
+      });
+
+      newTask.appendChild(line);
+      list.appendChild(newTask);
+
+      input.value = "";
+    }
+  }
+
+  if (todo > 0) {
+    document.getElementById("task-notification").innerHTML = "To Do";
+  } else {
+    document.getElementById("task-notification").innerHTML = "No tasks defined";
+  }
+};
+
+function addTaskDone(text) {
+  done++;
+  const doneTask = document.createElement("div");
+  doneTask.classList.add("task");
+
+  const line = document.createElement("div");
+  line.classList.add("content", "complete");
+  line.textContent = text;
+
+  doneTask.addEventListener("click", () => {
+    done--;
+    doneTask.remove();
+
+    if (done > 0) {
+      document.getElementById("task-done-notification").innerHTML = "Complete";
+    } else {
+      document.getElementById("task-done-notification").innerHTML = "";
+    }
+  });
+
+  doneTask.appendChild(line);
+  list_done.appendChild(doneTask);
+
+  if (done > 0) {
+    document.getElementById("task-done-notification").innerHTML = "Complete";
+  } else {
+    document.getElementById("task-done-notification").innerHTML = "";
+  }
+}
+
+form.addEventListener("keydown", addTask);
